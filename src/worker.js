9e46253b4f236557b80c32431a9732d6ba6c84d8
Mark Bennett <mark@burmis.ca>
2016-02-21 22:25:59 -0700

    Cache the main pahe resources.

--- src/worker.js
+++ src/worker.js
@@ -4,6 +4,16 @@ console.log("SW startup");
 
 self.addEventListener('install', function(event) {
   console.log("SW installed");
+
+  caches.open('request').then((cache) => {
+    cache.addAll([
+      '/',
+      '/index.bundle.js',
+      'https://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css',
+      'https://maps.google.com/maps/api/staticmap?center=53.5461361,-113.4991690&zoom=15&size=240x180&maptype=roadmap&sensor=false&language=&markers=color:green|label:none|53.5461361,-113.4991690',
+      'https://netdna.bootstrapcdn.com/bootstrap/3.0.0/fonts/glyphicons-halflings-regular.woff'
+    ]);
+  })
 });
 
 self.addEventListener('activate', function(event) {
@@ -12,4 +22,10 @@ self.addEventListener('activate', function(event) {
 
 self.addEventListener('fetch', function(event) {
   console.log("Caught a fetch!" + event.request.url);
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
 });
